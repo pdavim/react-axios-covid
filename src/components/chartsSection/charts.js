f2bb0470fa038543992d86ff81bf1a3d434c9452
Pedro Davim <pdavimmilkman@gmail.com>
2020-03-18 00:41:43 +0000

    form added

--- src/components/chartsSection/charts.js
+++ src/components/chartsSection/charts.js
@@ -258,6 +258,28 @@ const ContactText = () => (
     <h3>Contact</h3>
     <p>App development by Pedro Davim</p>
     <p>pdavim@pdavim.com</p>
+
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+
+      <p>
+        <label>
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </div>
 );
 
