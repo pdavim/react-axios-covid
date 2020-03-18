1480ad71468df7d57a44957cb56a3f630d747692
Pedro Davim <pdavimmilkman@gmail.com>
2020-03-18 01:21:46 +0000

    form correction

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -3,39 +3,63 @@ import NetlifyForm from "react-netlify-form";
 
 const ContactForm = () => {
   return (
-    <NetlifyForm name="Contact Form">
-      {({ loading, error, success }) => (
-        <div>
-          {loading && <div>Loading...</div>}
-          {error && (
-            <div>Your information was not sent. Please try again later.</div>
-          )}
-          {success && <div>Thank you for contacting us!</div>}
-          {!loading && !success && (
-            <div>
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" required />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your Email: <input type="email" name="email" required />
-                </label>
-              </p>
+    <div>
+      <NetlifyForm name="Contact Form">
+        {({ loading, error, success }) => (
+          <div>
+            {loading && <div>Loading...</div>}
+            {error && (
+              <div>Your information was not sent. Please try again later.</div>
+            )}
+            {success && <div>Thank you for contacting us!</div>}
+            {!loading && !success && (
+              <div>
+                <p>
+                  <label>
+                    Your Name: <input type="text" name="name" required />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Your Email: <input type="email" name="email" required />
+                  </label>
+                </p>
 
-              <p>
-                <label>
-                  Message: <textarea name="message" />
-                </label>
-              </p>
+                <p>
+                  <label>
+                    Message: <textarea name="message" />
+                  </label>
+                </p>
 
-              <button>Submit</button>
-            </div>
-          )}
-        </div>
-      )}
-    </NetlifyForm>
+                <button>Submit</button>
+              </div>
+            )}
+          </div>
+        )}
+      </NetlifyForm>
+
+      <form
+        name="contact"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        hidden
+      >
+        <p class="hidden">
+          <label>
+            Don’t fill this out if you're human: <input name="__bf" />
+          </label>
+        </p>
+        <p>
+          <input type="text" name="name" />
+        </p>
+        <p>
+          <input type="email" name="email" />
+        </p>
+        <p>
+          <textarea name="message" />
+        </p>
+      </form>
+    </div>
   );
 };
 
